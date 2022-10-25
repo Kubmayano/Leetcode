023be487e702b4bd805ef30bb50ab7eef21db64a
Kubmayano <elliot.cornier@outlook.com>
2022-10-25 20:39:28 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Basically just a github project for me to do Leetcode problems and hopefully improve over time, if you're scrolling
+    this at a later date then you can expect to find the harder problems at the bottom
+    and no before you ask these aren't sorted in any specific way (at least not yet)
+*/
+
+using System;
+
+namespace Leetcode
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            Solution s = new Solution();
+        }
+
+        public bool isPalindrome(int x) //Cheks if a number is palindrome, Question NO: 9
+        {
+            if (x < 0) return false;
+
+            string n = x.ToString();
+            string reverse = new String(n.ToString().Reverse().ToArray());
+
+            if (n == reverse) return true;
+            else return false;
+        }
+
+        public int TitleToNumber(string columnTitle) //Returns the integer value of excel rows, question NO: 171
+        {
+            return 0;
+        }
+    }
+}
